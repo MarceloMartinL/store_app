3d622db121d80dd41ebe75c0e56a6971c9bc131d
Marcelo <marcelo18a@msn.com>
2020-03-21 17:21:47 -0300

    add stores_controller methods, add namespace to stores routes, fix dockerfile

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,34 +1,41 @@
-module V1
-  class StoresController < ApplicationController
-    before_action :set_store, only: [:show, :update, :delete]
+module Api
+  module V1
+    class StoresController < ApplicationController
+      before_action :set_store, only: [:show, :update, :destroy]
 
-    def index
-      
-    end
-
-    def create
-      
-    end
-
-    def show
-      
-    end
-
-    def update
-      
-    end
-
-    def delete
-      
-    end
-
-    private
-      def store_params
-        params.require(:store).permit(:name, :address, :email, :phone)
+      def index
+        @stores = Store.all
+        json_response(@stores, :ok)
       end
 
-      def set_store
-        @store = Store.find(params[:id])
+      def create
+        @store = Store.new(store_params)
+        @store.save!
+        json_response(@store, :created)
       end
+
+      def show
+        json_response(@store, :ok)
+      end
+
+      def update
+        @store.update!(store_params)
+        json_response(@store, :ok)
+      end
+
+      def destroy
+        @store.destroy!
+        head :no_content
+      end
+
+      private
+        def store_params
+          params.require(:store).permit(:name, :address, :email, :phone)
+        end
+
+        def set_store
+          @store = Store.find(params[:id])
+        end
+    end
   end
 end
--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,6 +3,10 @@ module ExceptionHandler
 
   included do
     # Define custom handlers
+    rescue_from Exception do |e|
+      json_response({ message: e.message, backtrace: e.backtrace[0..10] }, :internal_server_error)
+    end
+
     rescue_from ActiveRecord::RecordInvalid do |e|
       json_response({ message: e.message }, :unprocessable_entity)
     end
